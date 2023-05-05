387bb79ed59f88b5bae0241c5b13d5f0ef28502d
Kanchan Pate <kpate@pgh.yearup.org>
2023-05-04 23:13:56 -0400

    working calculator for loan calculation

---
+++ scripts/main.js
@@ -0,0 +1,49 @@
+//Listen for submit action
+
+document
+  .getElementById("loan-form")
+  .addEventListener("submit", calculteResults);
+
+//Result calculations
+function calculteResults(e) {
+  console.log("calculating");
+
+  // bring in the ui variables
+  const amount = document.getElementById("amount");
+  const interest = document.getElementById("interest");
+  const years = document.getElementById("years");
+  const monthlyPayment = document.getElementById("monthly-payment");
+  const totalPayment = document.getElementById("total-payment");
+  const totalInterest = document.getElementById("total-interest");
+
+  //convert numbers to decimal before displaying results
+  const principal = parseFloat(amount.value);
+  const calculatedInterest = parseFloat(interest.value) / 100 / 12;
+  const calculatedPayment = parseFloat(years.value) * 12;
+
+  //calculating monthly payment
+  const x = Math.pow(1 + calculatedInterest, calculatedPayment);
+  const monthly = (principal * x * calculatedInterest) / (x - 1);
+
+  //
+  if (isFinite(monthly)) {
+    monthlyPayment.value = monthly.toFixed(2);
+    totalPayment.value = (monthly * calculatedPayment).toFixed(2);
+    totalInterest.value = (monthly * calculatedPayment - principal).toFixed(2);
+  } else {
+    showError("Please check entered number");
+  }
+
+  e.preventDefault();
+}
+function showError(error) {
+  const errorDiv = document.createElement("div");
+
+  //get element
+  const card = document.querySelector(".card");
+  const heading = document.querySelector(".heading");
+
+  errorDiv.className = "alert alert-danger";
+  errorDiv.appendChild(document.createTextNode(error));
+  Clipboard.insertBefore(errorDiv, heading);
+}
